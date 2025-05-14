5bab516821ae7f4800c579671fdaf2ad3cddcbb4
Alok <b22ci004@iitj.ac.in>
2025-05-14 15:52:11 +0530

    -

--- UI/voice-bot/src/App.jsx
+++ UI/voice-bot/src/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  
+  const [username , setUsername] = useState('')
+ 
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log(username + " submitted")
+    setUsername('')
+  }
+ 
   return (
     <div className='flex items-center justify-center min-h-screen'>
-      <form className='bg-gray-100 p-8 rounded-lg shadow-lg'>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className='bg-gray-100 p-8 rounded-lg shadow-lg'>
         <input 
           className='border p-4 text-lg mr-4' 
           type="text" 
           placeholder='Enter your name' 
-        />
+          onChange={(e)=> {
+            setUsername(e.target.value)
+          }}
+          value={username}
+          />
         <button 
           className='bg-blue-500 text-white px-6 py-3 text-xl rounded-lg 
-            hover:bg-blue-600 active:bg-blue-700 transition-all'>
+          hover:bg-blue-600 active:bg-blue-700 transition-all'>
           SUBMIT
         </button>
       </form>
