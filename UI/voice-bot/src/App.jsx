fff2df283443d184a79d9e9ec0d148bd01b0e50f
Alok <b22ci004@iitj.ac.in>
2025-05-14 11:15:16 +0530

    -

--- UI/voice-bot/src/App.jsx
+++ UI/voice-bot/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='flex items-center justify-center min-h-screen'>
+      <form className='bg-gray-100 p-8 rounded-lg shadow-lg'>
+        <input 
+          className='border p-4 text-lg mr-4' 
+          type="text" 
+          placeholder='Enter your name' 
+        />
+        <button 
+          className='bg-blue-500 text-white px-6 py-3 text-xl rounded-lg 
+            hover:bg-blue-600 active:bg-blue-700 transition-all'>
+          SUBMIT
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div>
   )
 }
 
